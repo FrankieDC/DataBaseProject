e1fc06072c1224a3c3a464b7c53337fa0ad7b8a7
FrankieDC <francisco.dechoudens@upr.edu>
2013-09-27 10:02:39 -0400

    Second Commit

---
+++ appjs/app.js
@@ -0,0 +1,25 @@
+var itemList = new Array(
+	new Item("Star Wars", "Episode 1", "1999", "Two Jedi Knights escape a hostile blokade...", "$22.00", "starwars.png"),
+	new Item("Megaman", "NT", "2003", "Join Mega Man EXE by...", "$10.00", "megaman.png")
+);
+//Se ejecuta antes de que la pagina con el id search se llame.
+$(document).on('pagebeforeshow', "#search", function( event, ui ) {
+	var len = itemList.length;
+	var list = $("#items-list");
+	list.empty();
+	var item;
+	for (var i=0; i < len; ++i){
+		item = itemList[i];
+		list.append("<li><a href=\"#\">" + 
+			"<img src="+ item.picture +">"+
+			"<center>" +
+			"<p>" + item.name + item.model + "</br>"+  item.year + "</br>"+ item.description + "</p>" + "</center>" +
+			"<p class=\"ui-li-aside\">" + item.price + "</p>" +
+			"</a></li>" 
+			);
+			
+	}
+	list.listview("refresh");
+});
+
+//<img src="generic_background.png" alt="" />
---
+++ appjs/item.js
@@ -0,0 +1,8 @@
+function Item(name, model, year, description, price, picture){
+	this.name = name;
+	this.model = model;
+	this.year = year;
+	this.description = description;
+	this.price = price;
+	this.picture = picture;
+}
